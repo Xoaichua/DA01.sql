09c7b69b48e50d35e842d5678c06d0754ff8ed5a
Xoaichua <164151570+Xoaichua@users.noreply.github.com>
2024-04-03 23:02:14 +0700

    Update Practice 4.sql

--- Practice 4.sql
+++ Practice 4.sql
@@ -15,3 +15,23 @@ when (x+y>z and y+z>x and x+z>y) then 'Yes'
 else 'No'
 end as triangle
 from Triangle
+Ex3
+SELECT 
+ROUND(100.0 * COUNT(case_id)/COUNT(*) FROM callers),1) AS call_percentage
+FROM callers
+WHERE call_category IS NULL 
+OR call_category = 'n/a'
+Ex4
+select name from Customer
+where referee_id is null or referee_id<>2
+Ex5
+SELECT
+survived,
+sum(case
+when pclass=1 then 1 else 0 end) as first_class,
+sum(case
+when pclass=2 then 1 else 0 end) as second_class,
+sum(case
+when pclass=3 then 1 else 0 end) as third_class
+FROM titanic
+group by survived
