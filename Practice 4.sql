aa6f178fd21bfbaff1f3474c30a11565fc1ed90b
Xoaichua <164151570+Xoaichua@users.noreply.github.com>
2024-04-03 20:25:58 +0700

    Update Practice 4.sql

--- Practice 4.sql
+++ Practice 4.sql
@@ -1,3 +1,4 @@
+Ex1 
 SELECT 
 sum(CASE
 when device_type='laptop' then 1 else 0
@@ -6,3 +7,11 @@ sum(CASE
 when device_type in ('tablet','phone') then 1 else 0
 end) as mobile_views
 FROM viewership
+Ex2
+select
+*,
+case
+when (x+y>z and y+z>x and x+z>y) then 'Yes'
+else 'No'
+end as triangle
+from Triangle
