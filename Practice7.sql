504bc2275966816a2b8b7ef8bdc5cc24a0677084
Xoaichua <164151570+Xoaichua@users.noreply.github.com>
2024-04-18 04:05:07 +0700

    Update Practice7.sql

--- Practice7.sql
+++ Practice7.sql
@@ -21,3 +21,54 @@ row_number() over(partition by user_id order by transaction_date
  asc) as rank
 FROM transactions) as a
 where rank=3
+Ex4
+with new_table as
+(SELECT transaction_date, user_id, product_id,
+rank() over (partition by user_id order by transaction_date desc) as rank
+FROM user_transactions) 
+select transaction_date, user_id, count(product_id) as purchase_count
+from new_table
+where rank=1
+group by transaction_date, user_id
+Ex5, neeed help
+with new_table as
+(SELECT user_id, tweet_date, tweet_count,
+lag(tweet_count) over (partition by user_id) as lag1,
+lag(tweet_count, 2) over (partition by user_id) as lag2
+FROM tweets)
+select 
+user_id, tweet_date,
+round((cast(tweet_count+lag1+lag2) as decimal/(1 + case when lag1!=0 then 1 else 0 end + case when lag2!=0 then 1 else 0 end)),2) as rolling_avg_3d
+from new_table
+Ex6
+with new_table as
+(SELECT merchant_id,credit_card_id, amount, 
+(transaction_timestamp-lag(transaction_timestamp) over(partition by merchant_id, credit_card_id, amount order by transaction_timestamp)) as mins
+FROM transactions)
+select count(*) as payment_count
+from new_table 
+where mins <= '00:10:00'
+Ex7
+with new_table AS
+(SELECT category, product, 
+sum(spend) as total_spend,
+row_number() over (partition by category order by sum(spend) desc) as rank
+FROM product_spend
+where extract(year from transaction_date)=2022
+group by category, product)
+select category, product, total_spend
+from new_table
+where rank =1 or rank=2
+Ex8
+with new_table as
+(SELECT a.artist_name,
+dense_rank() over (order by count(b.song_id) desc) as artist_rank
+FROM artists AS a
+join songs as b on a.artist_id=b.artist_id
+join global_song_rank as c on b.song_id=c.song_id
+where c.rank <=10
+group by a.artist_name)
+select artist_name, artist_rank
+from new_table
+where artist_rank <=5
+
