dcbf062b6171afc634a5deea715d4cbb04604ec0
Xoaichua <164151570+Xoaichua@users.noreply.github.com>
2024-04-23 14:36:49 +0700

    Update and rename Ex7.sql to Practice 8.sql

---
+++ Practice 8.sql
@@ -0,0 +1,9 @@
+Ex1
+SELECT 
+round(sum(case when order_date=customer_pref_delivery_date then 1 else 0 end)*100 / count(customer_id),2) as immediate_percentage
+from Delivery
+where (customer_id,order_date) in (
+ select customer_id, min(order_date) 
+FROM Delivery
+group by customer_id)
+Ex2
